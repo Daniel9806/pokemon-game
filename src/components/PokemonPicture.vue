a0cb5318e6c0a511126c9581bfc50d9093b77f4c
Daniel <80117423+Daniel9806@users.noreply.github.com>
2023-02-04 15:48:53 +0100

    first components

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex">
+        <img v-if="true" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         alt="pokemon" class="brightness-0">
+
+         <img v-if="false" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         alt="pokemon">
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+img {
+    position: absolute;
+    right: 20%;
+
+}
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="relative top-0">
+        <h2 class="font-bold text-2xl font-mono">What Pokemon is this?</h2>
+
+        <!-- TODO: img  -->
+        <PokemonPicture/>
+
+        <!-- TODO: options -->
+        <PokemonOptions/>
+
+        <!-- TODO: conform button -->
+
+    </div>
+</template>
+
+<script>
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+
+export default {
+    components: {
+        PokemonOptions,
+        PokemonPicture
+    },
+
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
